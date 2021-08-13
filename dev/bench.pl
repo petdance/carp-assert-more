c2c690797fb1b6653fecc2dc60c70501b4a717b2
Andy Lester <andy@petdance.com>
2021-08-13 11:31:25 -0500

    More tests

--- dev/bench.pl
+++ dev/bench.pl
@@ -11,6 +11,7 @@ use Carp::Assert::More;
 say version => $Carp::Assert::More::VERSION;
 
 use Benchmark;
+use DateTime;
 
 my @stooges = qw( Larry Curly Moe Shemp Joe );
 my %stooges = map { $_ => 1 } @stooges;
@@ -22,12 +23,15 @@ my @states = qw(
 );
 my %states = map { $_ => 1 } @states;
 
-timethis( 100_000,
+my $dt = DateTime->now();
+
+timethis( 1_000_000,
     sub {
         #assert_in( 'Shemp', \@stooges );
         #assert_keys_are( \%stooges, \@stooges );
         #    my $x = foo();
         #assert_positive_integer( 2112 );
-        assert_in( 'TX', \@states );
+        #assert_in( 'TX', \@states );
+        assert_isa_in( $dt, [ 'thing', 'DateTime' ] );
     }
 );
